32d4be21b8db23f17ecb133a4df82e6a120ed767
Felipe Tiepo <ftiepo20@gmail.com>
2024-04-02 16:35:48 -0300

    colocando forEach e innerHTML para fazer  card de cada vídeo

---
+++ app/mostrarVideos.js
@@ -0,0 +1,30 @@
+import { conectaApi } from "./conectaApi.js"
+
+                        //Dara atributes tem que colocar "[]"
+const lista = document.querySelector('[data-lista]')
+
+function constroiCard(titulo, descricao, url, imagem){
+    const video = document.createElement('li')
+    video.className = "videos__item"
+    video.innerHTML = `
+    <li class="videos__item">
+            <iframe width="100%" height="72%" src="${url}"
+                title="${titulo}" frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen></iframe>
+            <div class="descricao-video">
+                <img src="${imagem}" alt="logo canal alura">
+                <h3>${titulo}</h3>
+                <p>${descricao}</p>
+            </div>
+            `
+            
+    return video
+}
+
+async function listaVideo(){
+    const listaApi = await conectaApi.listaVideos()
+    listaApi.forEach(video => lista.appendChild(constroiCard(video.titulo, video.descricao, video.url, video.imagem)))
+}
+
+listaVideos()
