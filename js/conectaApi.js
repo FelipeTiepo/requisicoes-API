4aae3b8cc6caef5118c01eadf3da9ee41cf47bbe
Felipe Tiepo <ftiepo20@gmail.com>
2024-04-03 16:24:33 -0300

    adicionando o metodo de pesquisa com tratativas de erros

--- js/conectaApi.js
+++ js/conectaApi.js
@@ -1,31 +1,40 @@
-    async function listaVideos() {
-    //O await faz com que espere queo fetch acabe de executar para devolver o resultado da requisição
+async function listaVideos() {
     const conexao = await fetch("http://localhost:3000/videos");
-                                //FETCH retorna uma "Promise"
     const conexaoConvertida = await conexao.json();
 
     return conexaoConvertida;
 }
 
-async function criaVideo(titulo, descricao, url, imagem){
+async function criaVideo(titulo, descricao, url, imagem) {
     const conexao = await fetch("http://localhost:3000/videos", {
         method: "POST",
         headers: {
             "Content-type": "application/json"
         },
-        //Corpo da requisição, enviamos um objeto e o JSON.STRINGFY transforma ele em string
         body: JSON.stringify({
             titulo: titulo,
             descricao: `${descricao} mil visualizações`,
             url: url,
             imagem: imagem
         })
-    })  
-    const conexaoConvertida = await conexao.json()
-    return conexaoConvertida
+    });
+    if (!conexao.ok) {
+        throw new Error("Não foi possível enviar o vídeo")
+    }
+    const conexaoConvertida = conexao.json();
+
+    return conexaoConvertida;
+}
+
+async function buscaVideo(termoDeBusca) {
+    const conexao = await fetch(`http://localhost:3000/videos?q=${termoDeBusca}`);
+    const conexaoConvertida = await conexao.json();
+
+    return conexaoConvertida;
 }
 
 export const conectaApi = {
-    listaVideos, 
-    criaVideo
+    listaVideos,
+    criaVideo,
+    buscaVideo
 }
